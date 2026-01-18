2ae929bf81a7f170b0b91491ef4e21fdc8ba7615
thejosess <37021204+thejosess@users.noreply.github.com>
2026-01-18 19:17:28 +0100

    ADD change direction with limit using press A and press B

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    giro_a = direccion - 1
+    if (giro_a <= 0) {
+        giro_a = 4
+    }
+    direccion = giro_a
+})
+input.onButtonPressed(Button.B, function () {
+    giro_b = direccion + 1
+    if (giro_b >= 4) {
+        giro_b = 4
+    }
+    direccion = giro_b
+})
+let giro_b = 0
+let giro_a = 0
+let direccion = 0
 let pos_x = 0
 let pos_y = 4
-let direccion = 1
+direccion = 1
 basic.forever(function () {
     while (true) {
         led.plot(pos_x, pos_y)
@@ -10,11 +27,11 @@ basic.forever(function () {
             pos_x = pos_x - 1
             control.waitMicros(1000000)
         }
-        if (direccion == 3) {
+        if (direccion == 2) {
             pos_x = pos_x + 1
             control.waitMicros(1000000)
         }
-        if (direccion == 2) {
+        if (direccion == 3) {
             pos_y = pos_y + 1
             control.waitMicros(1000000)
         }
